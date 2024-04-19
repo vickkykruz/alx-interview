5e428813be8a4591fcf3acb96cd25103c0dfac59
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-19 14:52:34 +0100

    Testing

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,25 @@
 #!/usr/bin/node
-""" This is module prints all characters of a Star Wars movie """
-
 const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-const filmNum = process.argv[2] + '/';
-const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-// Makes an API request to get film information
-request(filmURL + filmNum, async function (err, res, body) {
-  if (err) return console.error(err);
-
-  // Parse the response body to get the list of character URLs
-  const charURLList = JSON.parse(body).characters;
-
-  // Iterare through the character URLs and fect character information
-  // Make a request to each character URL
-  for (const charURL of charURLList) {
-    await new Promise(function (resolve, reject) {
-      request(charURL, function (err, res, body) {
-        if (err) return console.error(err);
-
-        // Parse the charcter nformation and print the character's name Resolve the promise to indicate completion
-        console.log(JSON.parse(body).name);
-        resolve();
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
       });
-    });
+      console.log(res);
+    }
   }
 });
