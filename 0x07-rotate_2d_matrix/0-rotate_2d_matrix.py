005c2be1955e744189bc5d568703dcc67fa6a113
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-23 19:49:13 +0100

    Interview Rotate 2D Martix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix Module
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    This is a function that Rotate a 2D matrix 90 degrees clockwise.
+    """
+    for i in range(int(len(matrix) / 2)):
+        for j in range(i, (len(matrix) - i - 1)):
+            x = (len(matrix) - 1 - j)
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[x][i]
+            matrix[x][i] = matrix[(len(matrix) - i - 1)][x]
+            matrix[(len(matrix) - i - 1)][x] = matrix[j][(len(matrix) - i - 1)]
+            matrix[j][(len(matrix) - i - 1)] = tmp
