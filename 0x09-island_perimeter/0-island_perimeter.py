03081608d3abe25ae821a040f1027950171adb91
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-05-08 10:08:58 +0100

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Create a function def island_perimeter(grid): that returns the perimeter of
+the island described in grid:
+"""
+
+
+def island_perimeter(grid):
+    """ Island Perimeter """
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+    return perimeter
