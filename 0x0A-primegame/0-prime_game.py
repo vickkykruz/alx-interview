34a9b4ecdd8e4864f4b5a849064d34c8d522b765
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-05-16 20:46:09 +0000

    testing program

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,31 +7,56 @@ The player that cannot make a move loses the game.
 """
 
 
+def is_prime(num):
+    """ This is a function that return True if number is prime otherwise
+        false
+    """
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
 def isWinner(x, nums):
-    """This Prime Game returns the winner """
-    if x < 1 or not nums:
+    """ This function return the winner """
+    def find_next_prime(remaining):
+        """ This function return the prime number otherwise None """
+        for num in remaining:
+            if is_prime(num):
+                return num
         return None
 
-    m_wins = 0
-    b_wins = 0
+    def remove_multiples(prime, remaining):
+        """ This function return the list of number if is not divisable
+            by the prime number
+        """
+        return [num for num in remaining if num % prime != 0]
 
-    # generate a list of prime number based on the max numbers in num
-    n = max(nums)
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
+    maria_wins = 0
+    ben_wins = 0
 
-    for x in range(2, int(n**0.5) + 1):
-        if primes[x]:
-            for y in range(x**2, n + 1, x):
-                primes[y] = False
-
-    # count the no of pm less than n i nums
     for n in nums:
-        count = sum(primes[2:n+1])
-        b_wins += count % 2 == 0
-        m_wins += count % 2 == 1
+        remaining_numbers = list(range(1, n + 1))
+        maria_turn = True
 
-    if m_wins == b_wins:
+        while True:
+            next_prime = find_next_prime(remaining_numbers)
+            if next_prime is None:
+                break
+
+            remaining_numbers = remove_multiples(next_prime, remaining_numbers)
+            maria_turn = not maria_turn
+
+        if maria_turn:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
         return None
-
-    return 'Maria' if m_wins > b_wins else 'Ben'
